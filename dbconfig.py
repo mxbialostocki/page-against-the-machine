3d8ca2f284cdaf894e71b5fe6dc0292ade91c049
mxbialostocki <matt.bialostocki@gmail.com>
2020-11-15 15:37:27 +1300

    add db config file

---
+++ dbconfig.py
@@ -0,0 +1,10 @@
+"""
+Add your database configuration settings here and import them into the primary `settings.py` file in your Django installation
+"""
+
+DATABASE_ENGINE = ''
+DATABASE_NAME = ''
+DATABASE_USER = ''
+DATABASE_PASSWORD = ''
+DATABASE_HOST = ''
+DATABASE_PORT = ''
